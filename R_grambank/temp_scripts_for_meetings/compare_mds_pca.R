be8dfd453b4f1b66c05271367c0877c37d9dfe59
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-03-09 12:07:51 +0100

    Update compare_mds_pca.R

--- R_grambank/temp_scripts_for_meetings/compare_mds_pca.R
+++ R_grambank/temp_scripts_for_meetings/compare_mds_pca.R
@@ -6,18 +6,40 @@ Language_meta_data <-  read_csv(GRAMBANK_LANGUAGES, col_types=LANGUAGES_COLSPEC)
   distinct(Language_ID, .keep_all = T) %>% 
   mutate(Family_name = ifelse(is.na(Family_name), "Isolate", Family_name))
 
-GB_mds <- read_tsv(file.path("MDS", "MDS_table.tsv"))
+GB_mds <- read_tsv(file.path("MDS", "MDS_table.tsv")) %>% 
+  rename("MDS_classic_\nV1" = V1, "MDS_classic_\nV2" = V2)
 
+GB_MDS_non_metric <- read_tsv(file.path("MDS", "MDS_non-metric_table.tsv")) %>% 
+  rename("MDS_non_metric\n_V1" = V1, "MDS_non_metric\n_V2" = V2)
 
 GB_PCA_df <- suppressMessages(read_tsv(file.path("PCA", 'PCA_language_values.tsv'))) %>%
   dplyr::select(Language_ID, PC1, PC2, PC3) %>% 
   left_join(Language_meta_data, by = "Language_ID" ) %>%
   dplyr::select(Language_ID, everything())
 
-joined <- full_join(GB_mds, GB_PCA_df)
+joined <- full_join(GB_mds, GB_PCA_df) %>% 
+  full_join(GB_MDS_non_metric) %>% 
+  dplyr::select(PC1,  "MDS_classic_\nV1","MDS_non_metric\n_V1", PC2, "MDS_classic_\nV2","MDS_non_metric\n_V2")
+
+png("MDS/MDS_PCA_splom.png", height =20, width = 20, units = "cm", res = 400)
+pairs.panels(joined, 
+             method = "pearson", # correlation method
+             hist.col = "#a3afd1",# "#a9d1a3","",""),
+             density = TRUE,  # show density plots
+             ellipses = F, # show correlation ellipses
+             cex.labels= 1,
+             #           smoother= T,
+             cor=T,
+             lm=T,
+             ci = T, cex.cor = 0.9,stars = T
+)
+
+x <- dev.off()
+
+###
 
 joined %>% 
-  ggplot(aes(x = V1, y = PC1)) +
+  ggplot(aes(x = "MDS_classic_\nV1", y = PC1)) +
   geom_point() +
   geom_point(color = "turquoise3") +
   ggpubr::stat_cor(method = "pearson", p.digits = 2, geom = "label", color = "blue",
@@ -30,7 +52,7 @@ joined %>%
 ggsave("MDS/mds_PCA_1_plot.png")
 
 joined %>% 
-  ggplot(aes(x = V2, y = PC2)) +
+  ggplot(aes(x = "MDS_classic_\nV1", y = PC2)) +
   geom_point() +
   geom_point(color = "sienna1") +
   ggpubr::stat_cor(method = "pearson", p.digits = 2, geom = "label", color = "blue",
