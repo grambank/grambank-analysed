bd2416ad947eac8dad2750e6e24fce842b4e34a4
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-03-21 19:58:15 +0100

    Update INLA_featurewise_plotting.R

--- R_grambank/temp_scripts_for_meetings/INLA_featurewise_plotting.R
+++ R_grambank/temp_scripts_for_meetings/INLA_featurewise_plotting.R
@@ -1,7 +1,7 @@
 source("requirements.R")
 p_load(beepr)
 
-parameters <- read_csv("../../grambank_grambank/grambank/docs/feature_groupings/feature_grouping_for_analysis.csv", show_col_types = F, col_types = cols())
+parameters <- read_csv("feature_grouping_for_analysis.csv", show_col_types = F, col_types = cols()) 
 
 df_phylo_only <- readRDS("spatiophylogenetic_modelling/results/df_phylo_only.Rdata") %>%  
   left_join(parameters, by = "Feature_ID") %>% 
@@ -62,3 +62,17 @@ plot <- plot_df_summ_hed_subset %>%
 png( "spatiophylogenetic_modelling/figures/featurewise_dual_process_effects.png", width = 10, height = 25, units = "in", res = 100)
 plot(plot)
 x <- dev.off()
+
+simmo_plot_draft_1 <- plot_df_summ_hed_subset %>% 
+  reshape2::dcast(Feature_ID ~ effect, value.var = "Mean") %>% 
+  left_join(parameters, by = "Feature_ID") %>% 
+  ggplot() +
+  geom_point(aes(x = phylo, y = spatial, col = Main_domain)) +
+  ylim(c(0, 1)) +
+  xlim(c(0, 2.7)) +
+  theme_classic()
+
+png( "spatiophylogenetic_modelling/figures/featurewise_dual_process_effects_phylo_vs_spatial.png", width = 12, height = 12, units = "in", res = 100)
+plot(simmo_plot_draft_1)
+x <- dev.off()
+
