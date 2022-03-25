871c7ebd0e88f15a1eb2a03f093dfb3222e4a8af
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-03-25 14:59:07 +0100

    Update requirements.R

--- R_grambank/requirements.R
+++ R_grambank/requirements.R
@@ -149,3 +149,8 @@ sigma = c(1, 1.15) # Sigma parameter. First value is not used.
 #for waic_test.R and spatial_parameters.R
 kappa_vec = c(2, 4, 1, 2, 2)
 sigma_vec =  c(1.15, 2, 40, 10, 20)
+
+
+if (!dir.exists("output")) {
+  dir.create("output")
+}
