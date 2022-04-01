f2dfd4fd9b0d2c738807768ba49135db46e3efbc
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-04-01 11:11:41 +0200

    Update install_inla.R

--- R_grambank/spatiophylogenetic_modelling/install_inla.R
+++ R_grambank/spatiophylogenetic_modelling/install_inla.R
@@ -1,19 +1,32 @@
 source("requirements.R")
 
-# 1. Install BiocManager using: 
-pacman::p_load("BiocManager")
+# Check that INLA is installed
+if (!is_installed("INLA")) { 
+  cat("INLA wasn't installed, installing now.\n") 
+  # 1. Install BiocManager using: 
+  pacman::p_load("BiocManager")
+  
+  # 2. Install INLA dependencies with BiocManager using: 
+  BiocManager::install(c("graph","Rgraphviz",
+                         "rgdal",
+                         "rgl",
+                         "spdep"))
+  
+  # 3. Update foreach (although it unclear how vital this step was) using: 
+  #install.packages("foreach")
+  
+  # 4. Install INLA using: 
+  # NOTE: This is a big download
+  install.packages("INLA", repos=c(getOption("repos"), 
+                                   INLA="https://inla.r-inla-download.org/R/stable"), 
+                   dep=TRUE)
+  } else {
+  cat("Great, INLA was already installed, loading now.\n") 
+}
+suppressPackageStartupMessages(
+  library(INLA, quietly = T, warn.conflicts = F, verbose = F)
+)
 
-# 2. Install INLA dependencies with BiocManager using: 
-BiocManager::install(c("graph","Rgraphviz",
-                       "rgdal",
-                       "rgl",
-                       "spdep"))
 
-# 3. Update foreach (although it unclear how vital this step was) using: 
-#install.packages("foreach")
 
-# 4. Install INLA using: 
-# NOTE: This is a big download
-install.packages("INLA", repos=c(getOption("repos"), 
-                                 INLA="https://inla.r-inla-download.org/R/stable"), 
-                 dep=TRUE)
+
