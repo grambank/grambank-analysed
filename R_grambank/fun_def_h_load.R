ea9b35d34f3d4653142879d14a6a36dbd9b70d47
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-05-18 18:59:01 +0200

    improve h_load

--- R_grambank/fun_def_h_load.R
+++ R_grambank/fun_def_h_load.R
@@ -2,26 +2,34 @@
 #function to check if a pkg is installed or not, if not it installs it and either way it's loaded.
 #inspired by pacman::p_load()
 
-h_load <- function(pkg, verbose = F){
+h_load <- function(pkg, verbose = F, version = NULL, repos = "https://cloud.r-project.org"){
 #  p <- "ggpubr"
-#  pkg <- (geiger, ape)
-    for(p in pkg){
-      if(!(p %in% rownames(installed.packages()))){
+#  pkg <- c("geiger", "ape")
+      for(p in pkg){
+        
+      if(is.null(version) & (!(p %in% rownames(installed.packages())))){ #if no version is specified, check if it's installed and if not then go and install it as normal
         install.packages(p)
-
         if(verbose == T){
-                cat(paste0("Installed", p, ". "))}
-        
-          }
-      if(verbose == T){
-        library(p, character.only = T, quietly = F)
-              cat(paste0("Loaded ", p, ", version ",packageVersion(p)
-,".\n "))
-      }else{
-        
-        suppressMessages(library(p, character.only = T, quietly = T, verbose = F, warn.conflicts = F))}
-    
-      }
-  
-}
+          cat(paste0("Installed ", p, ".\n"))}
 
+      }
+      if(!is.null(version) & (!(p %in% rownames(installed.packages())))){    
+      
+      if(!"devtools"%in% rownames(installed.packages())){ #install devtools if it isn't already installed
+          install.packages(devtools)
+      }
+        library(devtools, quietly = T, verbose = F, warn.conflicts = F)
+        devtools::install_version(p, version = version, repos = repos)
+        
+        if(verbose == T){
+          cat(paste0("Installed ", p, ", version ", version, ".\n"))}
+      }
+      
+  if(verbose == T){
+    library(p, character.only = T, quietly = F)
+    cat(paste0("Loaded ", p, ", version ",packageVersion(p),".\n"))
+  }else{
+    suppressMessages(library(p, character.only = T, quietly = T, verbose = F, warn.conflicts = F))}
+  
+      }
+}
