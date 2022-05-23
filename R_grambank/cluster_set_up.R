b4abeef1ed7dd70e57de496353855c7e2d2644b1
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-05-23 10:06:25 +0200

    Update cluster_set_up.R

--- R_grambank/cluster_set_up.R
+++ R_grambank/cluster_set_up.R
@@ -3,3 +3,6 @@ r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
 
 options(Ncpus = 10)
+
+packageurl <- "https://cran.r-project.org/src/contrib/Archive/nloptr/nloptr_2.0.1.tar.gz"
+install.packages(packageurl, repos=NULL, type="source")
