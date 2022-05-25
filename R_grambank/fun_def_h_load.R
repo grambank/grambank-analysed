05c36bda3b37cd76b912f56ee0adf4ccbaf1a82c
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-05-25 10:41:10 +0200

    Update fun_def_h_load.R

--- R_grambank/fun_def_h_load.R
+++ R_grambank/fun_def_h_load.R
@@ -2,13 +2,15 @@
 #function to check if a pkg is installed or not, if not it installs it and either way it's loaded.
 #inspired by pacman::p_load()
 
-h_load <- function(pkg, verbose = F, version = NULL){
+h_load <- function(pkg, verbose = F, version = NULL, repos = "http://cran.us.r-project.org"){
 #  p <- "ggpubr"
 #  pkg <- c("geiger", "ape")
       for(p in pkg){
         
       if(is.null(version) & (!(p %in% rownames(installed.packages())))){ #if no version is specified, check if it's installed and if not then go and install it as normal
-        install.packages(p)
+        
+        install.packages(p, dependencies = T, repos = repos)
+        
         if(verbose == T){
           cat(paste0("Installed ", p, ".\n"))}
 
@@ -16,11 +18,11 @@ h_load <- function(pkg, verbose = F, version = NULL){
       if(!is.null(version) & (!(p %in% rownames(installed.packages())))){    
       
       if(!"devtools"%in% rownames(installed.packages())){ #install devtools if it isn't already installed
-          install.packages(devtools)
+          install.packages(devtools, dependencies = T, repos = repos)
       }
         
         library(devtools, quietly = T, verbose = F, warn.conflicts = F)
-        devtools::install_version(p, version = version)
+        devtools::install_version(p, version = version, dependencies = T, repos = repos)
         
         if(verbose == T){
           cat(paste0("Installed ", p, ", version ", version, ".\n"))}
