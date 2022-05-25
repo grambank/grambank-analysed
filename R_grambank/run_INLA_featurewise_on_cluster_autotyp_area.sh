cec77026c93c5f6b9dc7eb3e0ab30862ebb5417f
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-05-25 17:32:12 +0200

    Update run_INLA_featurewise_on_cluster_autotyp_area.sh

--- R_grambank/run_INLA_featurewise_on_cluster_autotyp_area.sh
+++ R_grambank/run_INLA_featurewise_on_cluster_autotyp_area.sh
@@ -4,19 +4,10 @@
 #SBATCH -J Hedvig_INLA_featurewise_autotyp_area
 #SBATCH --partition=dlcegpu
 
-#set ..rlibs as r package dir
-FILE=../rlibs
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-    echo "rlibs folder already exists, won't bother remaking it."
-else 
-mkdir ../rlibs
-fi
-
 export R_LIBS_USER=../rlibs
 
 #set up installation of scripts etc for the cluster.
-Rscript cluster_set_up.R
+#Rscript cluster_set_up.R
 
 #run INLA featurewise
 make INLA_featurewise_autotyp_area
