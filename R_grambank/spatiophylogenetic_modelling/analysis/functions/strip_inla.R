12c81ff5a699f0a16c91866febab2ab7ec6f2555
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-06-07 15:00:28 +0200

    rejig fun locations

---
+++ R_grambank/spatiophylogenetic_modelling/analysis/functions/strip_inla.R
@@ -0,0 +1,37 @@
+strip_inla = function(object){
+  
+  # Get the posterior of the ICC effect
+  icc_posterior = get_iccposterior(object, n = 100)
+  
+  # save the WAIC score
+  waic = object$waic
+  
+  list(icc_posterior = icc_posterior,
+       waic = waic)
+}
+
+get_iccposterior = function(object, n = 100){
+ 
+  hyper_sample = inla.hyperpar.sample(n = n,
+                       result = object)
+  
+  binomial_error = pi^2 / 3
+  
+  if(ncol(hyper_sample) == 1){
+    sigma = 1 / hyper_sample
+    
+    posterior = sigma / (sigma + 1 + binomial_error)
+  }
+  
+  if(ncol(hyper_sample) == 2){
+    sigma_1 = 1 / hyper_sample[,1]
+    sigma_2 = 1 / hyper_sample[,2]
+    
+    posterior_1 = sigma_1 / (sigma_1 + sigma_2 + 1 + binomial_error)
+    posterior_2 = sigma_2 / (sigma_1 + sigma_2 + 1 + binomial_error)
+    
+    posterior = cbind(posterior_1, posterior_2)
+    colnames(posterior) = colnames(hyper_sample)
+  }
+  posterior
+}
