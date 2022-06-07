d4a9f76d8dad6e845e4375ff643148a18457e6bf
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-06-07 15:10:48 +0200

    Update install_inla.R

--- R_grambank/spatiophylogenetic_modelling/install_inla.R
+++ R_grambank/spatiophylogenetic_modelling/install_inla.R
@@ -1,57 +1,21 @@
-R_version <- 4.2
-testing <- "yes"
-experimental <- "yes"
+verbose <- F
+
+if(verbose == T){
+  cat("For the analysis in this paper, we are using the testing-version of INLA, beta if you will, version = INLA_22.06.03. If you have another version of INLA running, please set it to this for maximum reproducibility.")
+  }
+
+
+if(!(INLA %in% rownames(installed.packages()))){
 
 source("fun_def_h_load.R")  
 
-h_load("BiocManager")
+h_load(pkg = c("Deriv", "Ecdat", "HKprocess", "gsl", "mlogit", "orthopolynom", "rgdal", "rgeos", "sn", "splancs"))
 
-if(!("spdep" %in% rownames(installed.packages()))){
-  # Install INLA dependencies with BiocManager using: 
-  BiocManager::install(c("graph","Rgraphviz",
-                         "rgdal",
-                         "rgl",
-                         "spdep")) 
-  }
-  
-  # Update foreach (although it unclear how vital this step was) using: 
- 
- h_load("foreach")
-  
-  # 4. Install INLA using: 
-  # NOTE: This is a big download
+install.packages("INLA",repos=c(INLA="https://inla.r-inla-download.org/R/testing"), dep=TRUE)
 
-#If inla isn't installed, we don't want the testing version and R is of a version 4.2 or higher, do this
-if (!("INLA" %in% rownames(installed.packages())) & testing != "yes" & R_version >=4.2) { 
-      cat("INLA wasn't installed, installing stable version now.\n") 
-    
-      install.packages("INLA", repos=c( 
-                                   INLA="https://inla.r-inla-download.org/R/stable"), 
-                   dep=TRUE)
-  }
-  
- #If inla isn't installed, we do want the testing version and R is of a version 4.2 or higher, do this
- if (!("INLA" %in% rownames(installed.packages())) & testing == "yes" & R_version >= 4.2){
-         install.packages("INLA",repos=c(INLA="https://inla.r-inla-download.org/R/testing"), dep=TRUE)
- }
- 
- #If inla isn't installed, we do want the testing version and R isn't of a version 4.2 or higher, do this 
- if (!("INLA" %in% rownames(installed.packages())) & testing == "yes" & R_version < 4.2){
-   h_load("remotes")
-   remotes::install_version("INLA", version="22.05.03",repos=c(INLA="https://inla.r-inla-download.org/R/testing"), dep=TRUE)
- }
- 
-#whatever you do, now load INLA
-suppressPackageStartupMessages(
-  library(INLA, quietly = T, warn.conflicts = F, verbose = F)
-  )
-
-
-
-if(experimental== "yes"){
-  INLA::inla.setOption(inla.mode="experimental")
 }
 
-cat(paste0("Loaded INLA version ", packageVersion("INLA"), ".\n"))
+library("INLA")
+INLA::inla.setOption(inla.mode="experimental")
 
 
