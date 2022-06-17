5c49efb3f5a7fd307bce97c2a29a0a8f0d49ad05
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-06-17 17:14:45 +0200

    Update predict_surprisal.R

--- R_grambank/unusualness/analysis/predict_surprisal.R
+++ R_grambank/unusualness/analysis/predict_surprisal.R
@@ -16,19 +16,23 @@ surprisal_fn <- paste0(OUTPUTDIR_tables, "/surprisal.tsv")
 if(!file.exists(surprisal_fn)){
   source("unusualness/analysis/get_unusualness_bayesLCA.R")
 }
+
+source("global_variables.R")
+source("spatiophylogenetic_modelling/analysis/INLA_parameters.R")
+source('spatiophylogenetic_modelling/analysis/functions/strip_inla.R')
+
+#read in data
 gb <- read.delim(file = surprisal_fn, sep = "\t") %>% 
   dplyr::select(Language_ID, aes, Surprisal, Estimator) %>% 
-  filter(Estimator == "Kernel 30")
+  filter(Estimator == "Kernel 30") %>% 
+  inner_join(lgs_in_analysis, by = "Language_ID") %>% #subset to the lgs where we have phylo prec matrices
+  mutate(Endangerement=ifelse(aes %in% c("threatened","moribund","nearly_extinct"),"endangered",aes)) # Recode endangerment
 
 ### NEXT PART REQUIRES MATRICES ETC
 
 #########################################
 ## (5) Model unusualness in terms of genealogical, areal covariates, and endangerement status
 #########################################
-
-# Recode endangerment
-gb<-gb %>%
-  mutate(Endangerement=ifelse(aes %in% c("threatened","moribund","nearly_extinct"),"endangered",aes))
 
 #In the spatiophylogenetic modelling of the features, we use the dataset cropped for missing data but without imputation. For the unsualness analsyis, we use the imputed data. They are different in the feature values, but it is the same subset of langauges in both. Therefore, we can use the same precision matrices for both the predict unsualness analysis and spatiophylogenetic modelling with INLA.
 precision_matrices_fn <- "output/spatiophylogenetic_modelling/processed_data/precision_matrices.RDS"
@@ -39,14 +43,28 @@ precision_matrices = readRDS(precision_matrices_fn)
 phylo_prec_mat = precision_matrices$phylogenetic_precision
 spatial_prec_mat = precision_matrices$spatial_precision
 
+#reading in AUTOTYP-area
+if (!file.exists("output/non_GB_datasets/glottolog_AUTOTYP_areas.tsv")) { s
+  source("unusualness/processing/assigning_AUTOTYP_areas.R") }		
+autotyp_area <- read.delim("output/non_GB_datasets/glottolog_AUTOTYP_areas.tsv", sep = "\t") %>%
+  dplyr::select(Language_ID, AUTOTYP_area_id_iid_model = AUTOTYP_area)
+
+data <- gb %>% 
+  left_join(autotyp_area, by = "Language_ID")
+
+x <- assert_that(all(data$Language_ID == lgs_in_analysis$Language_ID), msg = "Data doesn't match!")
+
+## Since we are using a sparse phylogenetic matrix, we need to math taxa to the correct
+## rows in the matrix
+data$phylo_id = match(data$Language_ID, rownames(phylo_prec_mat))
+data$spatial_id = match(data$Language_ID, rownames(spatial_prec_mat))
+data$obs_id = 1:nrow(data)
+
 #INLA phylo only
-source("spatiophylogenetic_modelling/analysis/INLA_parameters.R")
+#dual model
 source("spatiophylogenetic_modelling/install_inla.R")
 
-gb <- tree_tips_df %>% 
-  inner_join(gb, by =  "Language_ID")
-
-dual_model = inla(formula =Surprisal ~
+dual_model = INLA::inla(Surprisal ~
                     f(spatial_id,
                       model = "generic2",
                       Cmatrix = spatial_prec_mat,
@@ -55,34 +73,9 @@ dual_model = inla(formula =Surprisal ~
                       model = "generic2",
                       Cmatrix = phylo_prec_mat,
                       hyper = pcprior),
-                  data = gb,
+                  data = data,
                   control.family = list(hyper = list(prec = list(initial = log(1e+08), fixed = TRUE))))
 
 
-formula <- Surprisal~
-  (1 | gr(Glottocode, cov = spatial_covar_mat)) +
-  (1 | gr(Glottocode, cov = phylo_covar_mat))  (1|Family)+
-  Endangerement
 
-# Function that obtains a predictive model of surprisal
-model_surprisal<-function(df) {
-  m<-brm(formula = formula,
-         data=df,
-         chains = 4,
-         iter = 12000,
-         warmup = 5000,
-         cores = 4,
-         control = list(adapt_delta=0.99),
-         backend="cmdstanr")
-  return(m)}
-
-# From now on we centered our analyses on two estimates: LCA and kernel-20
-gb_redux<-gb %>%
-  filter(Estimator %in% c("prob_lca","prob_ker_20"))
-
-# Get models
-models_surprisal<-plyr::dlply(gb_redux,"Estimator",model_surprisal)
-
-# Check summary
-summary(model_surprisal_lca)
-conditional_effects(model_surprisal_lca)
+dual_model_stripped <- strip_inla(dual_model)
