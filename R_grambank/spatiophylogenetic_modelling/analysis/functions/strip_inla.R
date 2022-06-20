22dfaa47200a8903808098425e624bbaa7f329b3
rdinnager <r.dinnage@gmail.com>
2022-06-20 14:59:27 -0400

    extract marginal likelihood from inla object. probably easier to add normalising constant later, after running all model. we only need access to originla precision matrices for this.

--- R_grambank/spatiophylogenetic_modelling/analysis/functions/strip_inla.R
+++ R_grambank/spatiophylogenetic_modelling/analysis/functions/strip_inla.R
@@ -1,44 +1,53 @@
 strip_inla = function(object){
-  
+
 #  object <- phylo_only_model
   # try to get the posterior of the ICC effect
   icc_posterior <-  try(get_iccposterior(object, n = 100))
-  
+
   if ("try-error" %in% class(icc_posterior )) {
     cat(paste0("I failed in running strip_inla, most likely there's something wrong with the posteriors.\n"))
     icc_posterior  <- NULL
   }
-  
+
   # save the WAIC score
   waic = object$waic
   cpo =  object$cpo$cpo
   pit = object$cpo$pit
   cpo_failure = object$cpo$failure
+  ## get marginal likelihood, note this will need to have a normalising constant added to it later
+  ## this normalising constant only needs to be calculated once for each precision matrix
+  ## and is 0.5 * log, where det is the determinant of the precision matrix. If the model uses two
+  ## precision matrices both constants need to be added.
+  ## Let me know if you want to do the calculation here. It can be done but strip_inla() will
+  ## get a bit more complicated because the constant can't be extracted from the model object,
+  ## we will have to pass in the precision matrices as well (or the precalculated constants,
+  ## which would be better).
+  mlik <- object$mlik
 
   list(icc_posterior = icc_posterior,
-       waic = waic, cpi = cpo, pit = pit, cpo_failure = cpo_failure)
+       waic = waic, cpi = cpo, pit = pit, cpo_failure = cpo_failure, mlik = mlik)
 }
 
 get_iccposterior = function(object, n = 100){
- 
+
   hyper_sample = inla.hyperpar.sample(n = n,
                        result = object)
-  
+
   binomial_error = pi^2 / 3
-  
+
   if(ncol(hyper_sample) == 1){
     sigma = 1 / hyper_sample
-    
+
     posterior = sigma / (sigma + 1 + binomial_error)
   }
-  
+
   if(ncol(hyper_sample) == 2){
     sigma_1 = 1 / hyper_sample[,1]
     sigma_2 = 1 / hyper_sample[,2]
-    
+
     posterior_1 = sigma_1 / (sigma_1 + sigma_2 + 1 + binomial_error)
     posterior_2 = sigma_2 / (sigma_1 + sigma_2 + 1 + binomial_error)
-    
+
     posterior = cbind(posterior_1, posterior_2)
     colnames(posterior) = colnames(hyper_sample)
   }
