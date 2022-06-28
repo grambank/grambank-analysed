50f5670cbcf141bf16db5b7e221f1a0d93e9dfe2
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-06-28 23:10:41 +0200

    suprisal -> Surprisal

--- R_grambank/unusualness/analysis/plot_unusualness_worldmap.R
+++ R_grambank/unusualness/analysis/plot_unusualness_worldmap.R
@@ -1,7 +1,7 @@
 source("requirements.R")
 
-surprisal_df <-   read.delim(file = "output/unusualness/tables/surprisal.tsv", sep = "\t") %>% 
-  dplyr::select(Language_ID, surprisal, Probability)
+surprisal_df <-   read.delim(file = "output/unusualness/tables/surprisal.tsv", sep = "\t") %>%
+  dplyr::select(Language_ID, Surprisal, Probability)
 
 languages_df <- read_csv(GRAMBANK_LANGUAGES, col_types=LANGUAGES_COLSPEC) %>%		
   dplyr::select(Language_ID = Language_level_ID, Longitude, Latitude, Family_name, Name) %>% 
@@ -12,13 +12,13 @@ languages_df <- read_csv(GRAMBANK_LANGUAGES, col_types=LANGUAGES_COLSPEC) %>%
   ungroup %>%
   mutate(Family_group = if_else(n == 1, "Singleton", Family_group)) 
 
-limit <- quantile(surprisal_df$surprisal, 0.98, na.rm = T)  # top 2 %
+limit <- quantile(surprisal_df$Surprisal, 0.98, na.rm = T)  # top 2 %
 
 h <- surprisal_df %>% 
-  ggplot(aes(x = surprisal, fill=..x..)) +
+  ggplot(aes(x = Surprisal, fill=..x..)) +
   geom_histogram(bins = 50) +
   annotate("segment",col="black", alpha = 0.6, x = limit, xend = limit, y = 0, yend = 50, size = 0.5, linetype = "dashed") +
-  scale_fill_viridis('surprisal', option="D", direction = -1) +
+  scale_fill_viridis('Surprisal', option="D", direction = -1) +
   guides(fill="none") +
   xlab("Surprisal") + ylab("Number of Languages") +
   theme_classic() +
@@ -44,12 +44,12 @@ map <- ggplot(scores_joined_for_plotting) +
   geom_polygon(data=lakes, aes(x=long, y=lat, group=group),
                colour="gray87",
                fill="white", size = 0.3) +
-  geom_point(aes(x=Longitude, y=Latitude, color=surprisal), alpha = 0.6) +
+  geom_point(aes(x=Longitude, y=Latitude, color=Surprisal), alpha = 0.6) +
   geom_text_repel(
-    data=scores_joined_for_plotting[scores_joined_for_plotting$surprisal >= limit, ],
+    data=scores_joined_for_plotting[scores_joined_for_plotting$Surprisal >= limit, ],
     aes(x=Longitude, y=Latitude, label=Name),color="black", max.overlaps = 20
   ) +
-  scale_color_viridis('surprisal', option="D", direction = -1) +
+  scale_color_viridis('Surprisal', option="D", direction = -1) +
   theme(
     legend.position = "none",
     # all of these lines are just removing default things like grid lines, axes etc
@@ -69,32 +69,6 @@ map <- ggplot(scores_joined_for_plotting) +
 
 mh <- map + inset_element(h, right = 0.5, bottom = 0.0, left = 0.2, top = 0.4)
 
-png("output/unusualness/plots/DB_rarity.png", height = 5, width = 8, units = "in", res = 300)
+png("output/unusualness/plots/surprisal_worldmap.png", height = 5, width = 8, units = "in", res = 300)
 plot(mh)
-x <- dev.off()
-
-##old and new rarity
-
-scores <- read.delim("output/unusualness/tables/scores.tsv", sep = "\t") %>% 
-  dplyr::select(Language_ID = ID, score)
-
-both_df <- scores %>% 
-  full_join(surprisal_df, by = "Language_ID")
-
-both_df$score <- 1 - both_df$score
-
-cor.test(both_df$score, both_df$surprisal)
-
-scatterplot <- both_df %>% 
-ggplot(aes(x = surprisal, y = score)) +
-  geom_point(color = "#481567FF") +
-  ggpubr::stat_cor(method = "pearson", p.digits = 2, geom = "label", color = "blue",
-                   label.y.npc="top", label.x.npc = "left", alpha = 0.8) +
-  geom_smooth(method='lm', formula = 'y ~ x') +
-  theme_classic() +
-  labs(title="",		
-       x ="surprisal", y = "old rarity score") 
-
-png("output/unusualness/plots/rarity_vs_suprisal.png", height = 5, width = 5, units = "in", res = 300)
-plot(scatterplot)
 x <- dev.off()
