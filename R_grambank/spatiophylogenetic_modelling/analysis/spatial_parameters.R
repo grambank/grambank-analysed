9a2a3910ebda2e94ceea6a96aab24f336d7cd72a
Sam Passmore <passmore.sam@gmail.com>
2022-07-01 16:45:14 +1000

    working on spatial distance possiblities

--- R_grambank/spatiophylogenetic_modelling/analysis/spatial_parameters.R
+++ R_grambank/spatiophylogenetic_modelling/analysis/spatial_parameters.R
@@ -1,4 +1,4 @@
-# grounding distance
+# grounding covariance in distances
 #
 
 #script written by Sam Passmore
@@ -25,68 +25,125 @@ earth.dist <- function (long1, lat1, long2, lat2)
   return(d)
 }
 
-plotCircle <- function(LonDec, LatDec, Km, col = "black") {#Corrected function
-  #LatDec = latitude in decimal degrees of the center of the circle
-  #LonDec = longitude in decimal degrees
-  #Km = radius of the circle in kilometers
-  ER <- 6371 #Mean Earth radius in kilometers. Change this to 3959 and you will have your function working in miles.
-  AngDeg <- seq(1:360) #angles in degrees 
-  Lat1Rad <- LatDec*(pi/180)#Latitude of the center of the circle in radians
-  Lon1Rad <- LonDec*(pi/180)#Longitude of the center of the circle in radians
-  AngRad <- AngDeg*(pi/180)#angles in radians
-  Lat2Rad <-asin(sin(Lat1Rad)*cos(Km/ER)+cos(Lat1Rad)*sin(Km/ER)*cos(AngRad)) #Latitude of each point of the circle rearding to angle in radians
-  Lon2Rad <- Lon1Rad+atan2(sin(AngRad)*sin(Km/ER)*cos(Lat1Rad),cos(Km/ER)-sin(Lat1Rad)*sin(Lat2Rad))#Longitude of each point of the circle rearding to angle in radians
-  Lat2Deg <- Lat2Rad*(180/pi)#Latitude of each point of the circle rearding to angle in degrees (conversion of radians to degrees deg = rad*(180/pi) )
-  Lon2Deg <- Lon2Rad*(180/pi)#Longitude of each point of the circle rearding to angle in degrees (conversion of radians to degrees deg = rad*(180/pi) )
-  polygon(Lon2Deg,Lat2Deg,lty=1, lwd = 3, border = col)
-}
-
 #### Data ####
 languages <- read_csv(GRAMBANK_LANGUAGES, col_types=LANGUAGES_COLSPEC) %>%		
-  dplyr::select(Language_ID = Language_level_ID, Family_name, Name, Longitude, Latitude, Macroarea) %>% 
+  dplyr::select(Language_ID = Language_level_ID, 
+                Family_name, 
+                Name, 
+                Longitude, 
+                Latitude, 
+                Macroarea) %>% 
   distinct(Language_ID, .keep_all = T) 
 
 suppressWarnings(rownames(languages) <- languages$Language_ID)
 
-phylogeny = read.tree("output/spatiophylogenetic_modelling/processed_data/jaeger_pruned.tree")
+phylogeny = read.tree("output/spatiophylogenetic_modelling/processed_data/EDGE_pruned_tree.tree")
 
-#jitter points that are at exactly the same coordinates
-duplicate_coords = languages[duplicated(languages[,c("Longitude", "Latitude")]) | duplicated(languages[,c("Longitude", "Latitude")], fromLast = TRUE),"Language_ID"]
+# jitter points that are at exactly the same coordinates
+duplicate_coords = languages[
+  duplicated(languages[,c("Longitude", "Latitude")]) | 
+    duplicated(languages[,c("Longitude", "Latitude")], 
+               fromLast = TRUE), 
+  "Language_ID"]
+
 duplicate_rowid = languages$Language_ID %in% duplicate_coords
-languages$Latitude[duplicate_rowid] = jitter(languages$Latitude[duplicate_rowid], factor = 2)
-languages$Longitude[duplicate_rowid] = jitter(languages$Longitude[duplicate_rowid], factor = 2)
+
+languages$Latitude[duplicate_rowid] = 
+  jitter(languages$Latitude[duplicate_rowid], 
+         factor = 2)
+
+languages$Longitude[duplicate_rowid] = 
+  jitter(languages$Longitude[duplicate_rowid], 
+         factor = 2)
 
 # reorder to match phylogeny
 languages = languages[match(phylogeny$tip.label, languages$Language_ID),]
 
 #### Distance matrices ####
-## Phylogenetic
+
+## Phylogenetic distance based on branch lengths
 phylo_dist = vcv.phylo(phylogeny)
 
+## Spatial distance based on Haversine distances
 euclidean_dist = distm(languages[,c("Longitude", "Latitude")],
-                       fun = distHaversine)
+                       fun = distHaversine) / 1000
+
 dimnames(euclidean_dist) = list(languages$Language_ID, languages$Language_ID)
-# scale 
-euclidean_dist = scales::rescale(euclidean_dist)
 
 #### Make spatial matrix #####
 parameters = data.frame(kappa = kappa_vec,
                         sigma =  sigma_vec)
 
-spatial_parameters = map2(parameters$kappa, parameters$sigma,function(k, s){
-  spatial_covar_mat = varcov.spatial(languages[,c("Longitude", "Latitude")], cov.pars = c(1, s), kappa = k)$varcov
-  dimnames(spatial_covar_mat) = list(languages$Language_ID, languages$Language_ID)
-  spatial_covar_mat
+# Make the spatial covariance matrices from the set of parameters in parameters
+spatial_parameters = map2(parameters$kappa, 
+                          parameters$sigma,
+                          function(k, s){
+                            spatial_covar_mat = 
+                              varcov.spatial(
+                                languages[,c("Longitude", "Latitude")], 
+                                     cov.pars = c(1, s), 
+                                kappa = k)$varcov
+                            
+                            dimnames(spatial_covar_mat) = 
+                              list(languages$Language_ID,
+                                   languages$Language_ID)
+                            
+                            spatial_covar_mat
 })
 
+## What is the covariance value for a set of known pairs
+# we are interested in these distances (km)
+interest_distances = c(500, 1000, 2000, 4000)
+error_dist = 20
+# find pairs at approximately these distances
+dist_pairs = 
+  sapply(interest_distances,
+       function(id){
+         pair = which(euclidean_dist > id - error_dist & 
+                        euclidean_dist < id + error_dist, arr.ind = TRUE)[1,]
+         rownames(euclidean_dist)[pair] 
+       }
+)
+
+dist_pairs = data.frame(t(dist_pairs))
+colnames(dist_pairs) = c("first", "second")
+
+## Calculate those distances in KM
+distances = apply(dist_pairs, 1, function(x){
+  longlat1 = languages %>% dplyr::filter(Language_ID == x[1])
+  longlat2 = languages %>% dplyr::filter(Language_ID == x[2])
+  
+  earth.dist(longlat1$Longitude, longlat1$Latitude,
+             longlat2$Longitude, longlat2$Latitude)
+})
+dist_pairs$distances = round(distances, 2)
+
+# Calculate the covariance of pairs at varying distances
+covariances = lapply(spatial_parameters, function(s) 
+  diag(s[dist_pairs$first, dist_pairs$second]))
+covariances = do.call(cbind, covariances)
+covariances = round(covariances, 2)
+colnames(covariances) = paste("kappa", kappa_vec, "sigma", sigma_vec, sep = "_")
+
+dist_pairs = cbind(dist_pairs, covariances)
+
+
 #### Make plotting matrix ####
-# all_dist = c(1 - euclidean_dist[lower.tri(euclidean_dist)])
+# rescale euclidean distances to a 0 - 1 scale
+euclidean_dist = scales::rescale(euclidean_dist)
+
 paired_names = expand.grid(rownames(euclidean_dist), colnames(euclidean_dist))
 
 labs = paired_names[as.vector(upper.tri(euclidean_dist,diag=F)),]
+
 plot_df = cbind(labs, 1 - euclidean_dist[upper.tri(euclidean_dist,diag=F)])
 colnames(plot_df) = c("L1", "L2", "haversine_dist")
-plot_df$exp_dist = scales::rescale(exp(1 - euclidean_dist[upper.tri(euclidean_dist,diag=F)]))
+
+plot_df$exp_dist = scales::rescale(
+  exp(
+    1 - euclidean_dist[upper.tri(euclidean_dist,diag=F)]
+    )
+  )
 plot_df$phylo_dist = phylo_dist[upper.tri(phylo_dist,diag=F)]
 
 ## subset to a plottable set
@@ -103,21 +160,6 @@ spatialkappa_lines = lapply(spatial_parameters, function(x) {
   d[sample_idx]
 })
 
-distpair_names = data.frame(first = 
-                              c("gugu1253", "yami1254", "awar1249", "auuu1241"), 
-                            second = 
-                              c("juan1238", "kwin1241", "taul1251", "urim1252"))
-
-## Distances values
-distances = apply(distpair_names, 1, function(x){
-  longlat1 = languages %>% dplyr::filter(Language_ID == x[1])
-  longlat2 = languages %>% dplyr::filter(Language_ID == x[2])
-
-  earth.dist(longlat1$Longitude, longlat1$Latitude,
-             longlat2$Longitude, longlat2$Latitude)
-  })
-
-distances = round(distances, 2)
 
 cols = c("black", brewer.pal(7, "Set1"))
 
@@ -129,7 +171,7 @@ legend_text = c("1 - Haversine",
                 paste0("Spatial: k = ", parameters[4,1],"; s = ", parameters[4,2]))
 
 
-tiff("output/spatiophylogenetic_modelling/figures/spatial_varyingparameters.tiff", width = 8, height = 8, res = 400, units = "in")
+# tiff("output/spatiophylogenetic_modelling/figures/spatial_varyingparameters.tiff", width = 8, height = 8, res = 400, units = "in")
 plot(x = plot_ss$haversine_dist, y = plot_ss$haversine_dist, 
      type = "l", main = "Distance between all languages", 
      ylim = c(0, 1),
@@ -140,7 +182,6 @@ for(i in seq_along(spatialkappa_lines)){
     lines(x = plot_ss$haversine_dist, y = rev(spatialkappa_lines[[i]]), col = cols[i+2], lwd = 2)
 }
 
-lines(x = plot_ss$haversine_dist, y =  sort(plot_ss$phylo_dist), col = cols[2], lwd = 2)
 abline(v = c(0.6, 0.85, 0.9, 0.95), lty = "dashed")
 text(x =  c(0.6, 0.85, 0.9, 0.95) + 0.02, y = 0.5, labels = paste0(distances, " km"), srt = 90, font = 2)
 legend(0.05, 1.0, 
