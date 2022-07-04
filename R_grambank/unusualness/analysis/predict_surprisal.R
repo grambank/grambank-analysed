08f85d2c81a25e9cd3c1d8bd9137a89e4e73279e
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-07-04 11:22:38 +0200

    Update predict_surprisal.R

--- R_grambank/unusualness/analysis/predict_surprisal.R
+++ R_grambank/unusualness/analysis/predict_surprisal.R
@@ -17,11 +17,12 @@ if(!file.exists(surprisal_fn)){
   source("unusualness/analysis/get_unusualness_bayesLCA.R")
 }
 
+source('spatiophylogenetic_modelling/analysis/functions/varcov_spatial.R')
+
 #read in data
 gb <- read.delim(file = surprisal_fn, sep = "\t") %>% 
   dplyr::select(Language_ID, aes, Surprisal, Estimator) %>% 
   filter(Estimator == "Kernel 30") %>% 
-  inner_join(lgs_in_analysis, by = "Language_ID") %>% #subset to the lgs where we have phylo prec matrices
   mutate(Endangerement=ifelse(aes %in% c("threatened","moribund","nearly_extinct"),"endangered",aes)) # Recode endangerment
 
 ### NEXT PART REQUIRES MATRICES ETC
@@ -30,13 +31,44 @@ gb <- read.delim(file = surprisal_fn, sep = "\t") %>%
 ## (5) Model unusualness in terms of genealogical, areal covariates, and endangerement status
 #########################################
 
+#### Phylogenetic Precision ####
+tree_fn <- "output/spatiophylogenetic_modelling/processed_data/EDGE_pruned_tree.tree"
+if(!(file.exists(tree_fn))){
+  source("spatiophylogenetic_modelling/processing/pruning_EDGE_tree.R")}
+phylogenetic_tree = read.tree(tree_fn)
 
-phylogenetic_tree <- phylogenetic_tree %>% keep.tip(inner_joined_df$Glottocode)
+#subsetting gb to those with matches in the tree and subsetting the tree to those with matches in gb
+phylogenetic_tree_tips_df <- phylogenetic_tree$tip.label %>% 
+  as.data.frame() %>% 
+  rename(Language_ID = ".")
+
+gb <- gb %>% 
+  inner_join(phylogenetic_tree_tips_df, by = "Language_ID")
+  
+phylogenetic_tree <- ape::keep.tip(phylogenetic_tree, gb$Language_ID)
 
 #brms
 #making a covariance matrix of the tree
 vcv_tree <- vcv.phylo(phylogenetic_tree)
 
+vcv_tree  %>% 
+  qs::qsave("output/unusualness/tables/vcv_tree.qs")
+
+#spatial vcv
+
+kappa = 2 # smoothness parameter as recommended by Dinnage et al. (2020)
+sigma = c(1, 1.15) # Sigma parameter. First value is not used. 
+
+# Get the longitude and latitude data from the simulated datasets
+locations_df = read.delim('output/non_GB_datasets/glottolog-cldf_wide_df.tsv', sep = "\t") %>%
+  inner_join(gb, by = "Language_ID") #subset to matches in tree and in cropped in GB.
+
+spatial_covar_mat = varcov.spatial(locations_df[,c("Longitude", "Latitude")],
+                                   cov.pars = sigma,
+                                   kappa = kappa)$varcov
+
+spatial_covar_mat  %>% 
+  qs::qsave("output/unusualness/tables/spatial_covar_mat.qs")
 
 ###BRMS
 
