8338569317621c4bc6956d0548014112edb12466
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-07-06 22:06:55 +0200

    change in line with #55

--- R_grambank/spatiophylogenetic_modelling/analysis/INLA_parameters.R
+++ R_grambank/spatiophylogenetic_modelling/analysis/INLA_parameters.R
@@ -3,8 +3,8 @@ kappa = 2 # smoothness parameter as recommended by Dinnage et al. (2020)
 sigma = c(1, 1.15) # Sigma parameter. First value is not used. 
 
 #for waic_test.R and spatial_parameters.R
-kappa_vec = c(2, 4, 1, 2, 2)
-sigma_vec =  c(1.15, 2, 40, 10, 20)
+kappa_vec = c(2, 2, 2.5)
+sigma_vec =  c(1.15, 2, 3)
 
 #### Set up model priors ####
 
