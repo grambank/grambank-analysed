cdd98a9065f74626f1e60d1cecebd398e34d5227
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-07-06 18:47:19 +0200

    Update plot_unusualness_worldmap.R

--- R_grambank/unusualness/analysis/plot_unusualness_worldmap.R
+++ R_grambank/unusualness/analysis/plot_unusualness_worldmap.R
@@ -2,7 +2,7 @@ source("requirements.R")
 
 surprisal_df <-   read.delim(file = "output/unusualness/tables/surprisal.tsv", sep = "\t") %>%
   dplyr::select(Language_ID, aes, Surprisal, Estimator, Probability) %>% 
-  filter(Estimator == "Kernel 30") %>% 
+  filter(Estimator == "Kernel 20") %>% 
   dplyr::select(Language_ID, Surprisal, Probability)
 
 languages_df <- read_csv(GRAMBANK_LANGUAGES, col_types=LANGUAGES_COLSPEC) %>%		
@@ -20,7 +20,7 @@ h <- surprisal_df %>%
   ggplot(aes(x = Surprisal, fill=..x..)) +
   geom_histogram(bins = 50) +
   annotate("segment",col="black", alpha = 0.6, x = limit, xend = limit, y = 0, yend = 50, size = 0.5, linetype = "dashed") +
-  scale_fill_viridis('Surprisal', option="D", direction = -1) +
+  scale_fill_viridis('Surprisal', option="A", direction = -1) +
   guides(fill="none") +
   xlab("Surprisal") + ylab("Number of Languages") +
   theme_classic() +
@@ -51,7 +51,7 @@ map <- ggplot(scores_joined_for_plotting) +
     data=scores_joined_for_plotting[scores_joined_for_plotting$Surprisal >= limit, ],
     aes(x=Longitude, y=Latitude, label=Name),color="black", max.overlaps = 20
   ) +
-  scale_color_viridis('Surprisal', option="D", direction = -1) +
+  scale_color_viridis('Surprisal', option="A", direction = -1) +
   theme(
     legend.position = "none",
     # all of these lines are just removing default things like grid lines, axes etc
@@ -67,9 +67,9 @@ map <- ggplot(scores_joined_for_plotting) +
     axis.ticks = element_blank()
   ) +
   coord_map(projection = "vandergrinten", ylim=c(-56,67)) +
-  expand_limits(x = scores_joined_for_plotting$Longitude, y = scores_joined_for_plotting$Latitude)
+  expand_limits(x = scores_joined_for_plotting$Longitude, y = scores_joined_for_plotting$Latitude) 
 
-mh <- map + inset_element(h, right = 0.5, bottom = 0.0, left = 0.2, top = 0.4)
+mh <- map + inset_element(h, right = 0.2, bottom = 0.0, left = 0.0, top = 0.3)
 
 png("output/unusualness/plots/surprisal_worldmap.png", height = 5, width = 8, units = "in", res = 300)
 plot(mh)
