55eeb1dc0430381066aaa3921b64f00f222cd169
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-08-18 16:14:10 +0200

    splitting INLA results plots into separate scripts

---
+++ R_grambank/spatiophylogenetic_modelling/analysis/featurewise/INLA_results_most_prep.R
@@ -0,0 +1,103 @@
+source('requirements.R')
+
+source("spatiophylogenetic_modelling/analysis/INLA_parameters.R")
+pcprior <- prior_ten_percent
+
+OUTPUTDIR <- "output/spatiophylogenetic_modelling/INLA_ASR/"
+if(!dir.exists(OUTPUTDIR)){
+  dir.create(OUTPUTDIR)
+}
+
+beep = 1
+
+#description of features
+GB_id_desc <- readr::read_tsv("output/GB_wide/parameters_binary.tsv", show_col_types = F) %>%
+  dplyr::select(Feature_ID = ID, Grambank_ID_desc, Name)
+
+source("spatiophylogenetic_modelling/analysis/INLA_parameters.R")
+
+cat("\n###\nLoading covariance matrices...\n")
+
+precision_matrices_fn <- "output/spatiophylogenetic_modelling/processed_data/precision_matrices_kappa_2_sigma_1.15.RDS"
+if(!(file.exists(precision_matrices_fn))){
+  source("spatiophylogenetic_modelling/analysis/make_precisionmatrices.R")}
+
+precision_matrices = readRDS(precision_matrices_fn)
+phylo_prec_mat = precision_matrices$phylogenetic_precision
+spatial_prec_mat = precision_matrices$spatial_precision
+
+color_vector <-c("#593d9cff", "#f68f46ff", "#d6d4d4")
+
+#### Format Posterior Data ####
+## Read in model posteriors
+model_output_files = list.files(
+  #path = "output/spatiophylogenetic_modelling/featurewise/",
+  path = "/Users/skirgard/Nextcloud/Git_output/grambank-analysed/output_old/spatiophylogenetic_modelling/featurewise/",
+  pattern = ".*kappa_2_sigma_1.15_pcprior0.1.*.qs",
+  full.names = TRUE)
+
+model_output = lapply(model_output_files, qread)
+
+names(model_output) = basename(model_output_files) %>%
+  tools::file_path_sans_ext(.)
+
+## Extract the posterior distrubtions for each dual_process model from the hypersample
+dual_posterior = lapply(model_output, function(m) {
+  dd = m[[1]]$hyper_sample
+  binomial_error = pi^2 / 3
+  # Calculate h^2
+  posterior = (1 / dd) / (rowSums(1 / dd) + 1 + binomial_error)
+  
+  posterior
+})
+# Convert this to a dataframe
+dual_posterior = map_df(dual_posterior, ~as.data.frame(.x), .id="id")
+
+dual_hyperpar = lapply(model_output, function(m) {
+  dd = apply(m[[1]]$hyper_sample, 2, function(x) median(log(x)))
+  dd
+})
+
+dual_hyperpar <- do.call(rbind, dual_hyperpar) %>%
+  as.data.frame()
+dual_hyperpar$Feature_ID_model <- rownames(dual_hyperpar)
+
+colnames(dual_posterior) = c(
+  "Feature_ID_model",
+  "spatial",
+  "phylogenetic"
+)
+
+dual_posterior$Feature_ID <- str_extract(dual_posterior$Feature_ID_model, "GB[0-9]{3}a?b?")
+dual_hyperpar$Feature_ID <- str_extract(dual_hyperpar$Feature_ID_model, "GB[0-9]{3}a?b?")
+
+# join feature metadata to posterior
+dual_posterior = left_join(dual_posterior, GB_id_desc, by ="Feature_ID") 
+
+# Summarise the posterior distributions
+dual_summary = dual_posterior %>%
+  group_by(Feature_ID) %>%
+  summarise(mean_phylogenetic = mean(phylogenetic),
+            mean_spatial = mean(spatial),
+            error_phylogenetic = sd(phylogenetic),
+            error_spatial = sd(spatial),
+            #            domain = first(Main_domain),
+            cor = list(cor(cbind(phylogenetic, spatial))),
+            median_phylogenetic = median(phylogenetic),
+            median_spatial = median(spatial)) %>%
+  arrange(desc(mean_phylogenetic)) %>%
+  left_join(GB_id_desc, by = "Feature_ID")
+
+glottolog_df <- read_tsv("output/non_GB_datasets/glottolog-cldf_wide_df.tsv", show_col_types = F) %>%
+  mutate(Family_ID = ifelse(is.na(Family_ID), "Isolate", Family_ID))
+
+#reading in GB
+GB_fn <- "output/GB_wide/GB_cropped_for_missing.tsv"
+if(!file.exists(GB_fn)){
+  cat(paste0("Making GB data wide, binarising, cropping etc..\n"))
+  source("make_wide.R")
+  source("make_wide_binarized.R")
+  source("impute_missing_values.R")
+}
+GB_df <- readr::read_tsv(file =   GB_fn,show_col_types = F) %>%
+  inner_join(lgs_in_analysis, by = "Language_ID")
---
+++ R_grambank/spatiophylogenetic_modelling/analysis/featurewise/INLA_results_most_spatial_maps.R
@@ -0,0 +1,90 @@
+source("spatiophylogenetic_modelling/analysis/featurewise/INLA_results_most_prep.R")
+
+#making map plots to show the feature that has the most spatial effect in the model
+
+five_most_spatial_features <- dual_summary %>%
+  top_n(n = 5, wt = mean_spatial) %>%
+  arrange(desc(mean_spatial)) %>%
+  dplyr::select(Feature_ID) %>%
+  as.matrix() %>%
+  as.vector()
+
+#read in meta data
+Language_meta_data_fn <- "output/non_GB_datasets/glottolog-cldf_wide_df.tsv"
+if(!file.exists(Language_meta_data_fn)){
+  source("make_glottolog-cldf_table.R")
+}
+Language_meta_data  <- read_tsv(Language_meta_data_fn, show_col_types = F) %>%
+  dplyr::select(Language_ID, Longitude, Latitude)
+
+
+df_for_maps <- GB_df %>%
+  inner_join(Language_meta_data, by = "Language_ID") %>%
+  mutate(Longitude = if_else(Longitude <= -25, Longitude + 360, Longitude)) #shift for pacific
+
+#basemap
+#rendering a worldmap that is pacific centered
+world <- map_data('world', wrap=c(-25,335), ylim=c(-56,80), margin=T)
+lakes <- map_data("lakes", wrap=c(-25,335), col="white", border="gray", ylim=c(-55,65), margin=T)
+
+basemap <- ggplot(df_for_maps) +
+  geom_polygon(data=world, aes(x=long, y=lat, group=group),
+               colour="gray87",
+               fill="gray87", size = 0.5) +
+  geom_polygon(data=lakes, aes(x=long, y=lat, group=group),
+               colour="gray87",
+               fill="white", size = 0.3) +
+  theme(
+    # all of these lines are just removing default things like grid lines, axes etc
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.line = element_blank(),
+    panel.border = element_rect(colour = "black", fill = NA),
+    panel.background = element_rect(fill = "white"),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank()
+  ) +
+  coord_map(projection = "vandergrinten", ylim=c(-56,67)) +
+  theme(plot.margin=grid::unit(c(0,0,0,0), "mm"))
+
+index <- 0
+
+for(feature in five_most_spatial_features){
+  
+  #  feature <- five_most_spatial_features[1]
+  
+  index <- index + 1
+  #generating plot title
+  plot_title <- GB_id_desc %>%
+    filter(Feature_ID == feature) %>%
+    dplyr::select(Name) %>%
+    as.matrix() %>%
+    as.vector()
+  
+  plot_title <- paste0(feature, " ", plot_title)
+  plot_title <- gsub('(.{1,60})(\\s|$)', '\\1\n', plot_title)
+  
+  #filename
+  filename <- paste(OUTPUTDIR, "/most_signal_spatial_",index, "_" , feature, ".png", sep = "")
+  
+  feature_df <-df_for_maps %>%
+    filter(!is.na("Longitude")) %>%
+    dplyr::select(Language_ID, all_of(feature), Longitude, Latitude) %>%
+    rename(Feature = 2) %>%
+    mutate(point.color = as.character(Feature)) %>%
+    mutate(point.color = str_replace_all(point.color, "1", color_vector[1])) %>%
+    mutate(point.color = str_replace_all(point.color, "0", color_vector[2])) %>%
+    mutate(point.color = ifelse(is.na(point.color), color_vector[3], point.color)) %>%
+    arrange(Feature)
+  
+  basemap +
+    geom_jitter(data = feature_df, mapping = aes(x = Longitude, y = Latitude),  color = feature_df$point.color, alpha = 0.6, width = 3) +
+    ggtitle(plot_title) +
+    theme(plot.title = element_text(size = 25, face = "bold"))
+  
+  ggsave(filename = filename, width = 9.3, height = 9.2, units = "in")
+  
+}
