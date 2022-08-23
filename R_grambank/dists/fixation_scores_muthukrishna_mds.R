c50167621e8859c9acd2508499400465fbfff964
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-08-23 19:12:21 +0200

    Update fixation_scores_muthukrishna_mds.R

--- R_grambank/dists/fixation_scores_muthukrishna_mds.R
+++ R_grambank/dists/fixation_scores_muthukrishna_mds.R
@@ -1,17 +1,35 @@
 source("requirements.R")
 
-Language_meta_data <-  read_tsv("output/non_GB_datasets/glottolog-cldf_wide_df.tsv", col_types = cols()) %>% 
+Language_meta_data <-  read_tsv("output/non_GB_datasets/glottolog-cldf_wide_df.tsv", show_col_types = F) %>% 
   dplyr::select(Language_ID, level, Family_ID, Name, Macroarea) %>% 
-  mutate(Family_ID = ifelse(is.na(Family_ID), "Isolate", Family_ID)) %>% 
-  mutate(Macroarea = ifelse(Family_ID == "Isolate", "Isolate", Macroarea)) %>% 
-  filter(!is.na(Macroarea)) %>% 
-  filter(level == "language") %>% 
-    group_by(Family_ID, Macroarea) %>%
+  mutate(Family_ID = ifelse(is.na(Family_ID), "Isolate", Family_ID)) 
+#  filter(!is.na(Macroarea)) %>% 
+#  filter(level == "language") %>% 
+#    group_by(Family_ID, Macroarea) %>%
+#  summarise(n = n(), .groups = "drop") %>% 
+#  arrange(desc(n)) %>% 
+#  distinct(Family_ID, .keep_all = T)
+
+#sometimes languages in an autotyp-area are not classified all as the same macroarea. we take the macroarea per autotyp-area that is the most common. for example, N Coast Asia has 58 languages glottolog puts in Eurasia and 2 in North Americas. We assign N Coast Asia to Eurasia macroarea
+
+autotyp_area <- read_tsv("output/non_GB_datasets/glottolog_AUTOTYP_areas.tsv", col_types = cols()) %>%
+  dplyr::select(Language_ID, AUTOTYP_area)
+
+macroarea_per_autotyp_area <- Language_meta_data %>% 
+  left_join(autotyp_area, by = "Language_ID") %>% 
+  group_by(AUTOTYP_area, Macroarea) %>% 
   summarise(n = n(), .groups = "drop") %>% 
-  arrange(desc(n)) %>% 
-  distinct(Family_ID, .keep_all = T)
+  full_join(autotyp_area, by = "AUTOTYP_area") %>% 
+  group_by(Language_ID, AUTOTYP_area) %>% 
+  slice_max(n) %>% 
+  dplyr::select(Language_ID, AUTOTYP_area, Macroarea)
   
-df_long <- read_tsv("output/dists/cfx_Macroarea_cut_off_0_list.tsv", show_col_types = F) %>% 
+Language_meta_data <- Language_meta_data  %>% 
+  dplyr::select(-Macroarea) %>% 
+  left_join(macroarea_per_autotyp_area, by = "Language_ID") %>% 
+  mutate(americas = ifelse(str_detect(Macroarea, "merica"), "americas", "not americas"))
+
+df_long <- read_tsv("output/dists/cfx_AUTOTYP_area_cut_off_0_list.tsv", show_col_types = F) %>% 
   separate("Vars", into = c("Var1", "Var2"), sep = " - ")
 
 #recover symmetric distance matrix
@@ -29,15 +47,17 @@ mds_df <- mds$points %>%
 # The palette with black:
 cbbPalette <- c( "#E69F00", "#009E73", "#F0E442", "#D55E00", "#CC79A7","#000000")
 
-
 mds_plot <- mds_df %>% 
-  ggplot(aes(x = V1, y = V2)) +
+  left_join(Language_meta_data, by = c("Label" = "AUTOTYP_area")) %>% 
+  distinct(Label, Macroarea, V1, V2, americas) %>% 
+  ggplot(aes(x = V1, y = V2, color = americas)) +
   geom_point() +
-  geom_text(aes(label = Label)) +
+  geom_label_repel(aes(label = Label)) +
   theme_minimal() +
-  coord_fixed() +
-  scale_colour_manual(values=cbbPalette)
+  coord_fixed() 
 
-png("output/dist_fixation_scores/MDS_plot_family_cutoff_10.png", width = 6, height = 6, units = "in", res = 300)
+mds_plot
+
+png("output/dists/MDS_plot_autotyp_area.png", width = 12, height = 12, units = "in", res = 300)
 plot(mds_plot)
 x <- dev.off()
