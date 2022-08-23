24d564eb7d2315ba159fa76fb184bf9e3b164a5e
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-08-23 15:38:58 +0200

    Update fixation_scores_muthukrishna_americas.R

--- R_grambank/dists/fixation_scores_muthukrishna_americas.R
+++ R_grambank/dists/fixation_scores_muthukrishna_americas.R
@@ -1,2 +1,7 @@
 
-source("dists/fixation_scores_muthukrishna_CLI.R")
+source("dists/fixation_scores_muthukrishna_GB_fun.R")
+
+Language_meta_data <- Language_meta_data %>% 
+  mutate(americas = ifelse(str_detect(Macroarea, "merica"), "America", "Not americas"))
+
+fun_cfx(df = Language_meta_data, group = "Macroarea")
