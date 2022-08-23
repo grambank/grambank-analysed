467d273d7dcdf28800df3d812685d590fe47e679
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-08-23 15:39:07 +0200

    Update fixation_scores_muthukrishna_mds.R

--- R_grambank/dists/fixation_scores_muthukrishna_mds.R
+++ R_grambank/dists/fixation_scores_muthukrishna_mds.R
@@ -11,29 +11,29 @@ Language_meta_data <-  read_tsv("output/non_GB_datasets/glottolog-cldf_wide_df.t
   arrange(desc(n)) %>% 
   distinct(Family_ID, .keep_all = T)
   
-df_long <- read_tsv("output/dist_fixation_scores/cfx_Family_ID_cut_off_1_list.tsv", show_col_types = F) %>% 
+df_long <- read_tsv("output/dists/cfx_Macroarea_cut_off_0_list.tsv", show_col_types = F) %>% 
   separate("Vars", into = c("Var1", "Var2"), sep = " - ")
 
 #recover symmetric distance matrix
 h_load("igraph")
 g <- igraph::graph.data.frame(df_long, directed=FALSE)
 
-Family_ID_matrix <- igraph::get.adjacency(g, attr="Value_cfx", sparse=FALSE)
+Label_matrix <- igraph::get.adjacency(g, attr="Value_cfx", sparse=FALSE)
 
-mds <- MASS::isoMDS(Family_ID_matrix)
+mds <- MASS::isoMDS(Label_matrix)
 
 mds_df <- mds$points %>% 
   as.data.frame() %>% 
-  rownames_to_column("Family_ID") %>% 
-  left_join(Language_meta_data, by = "Family_ID")
+  rownames_to_column("Label") 
 
 # The palette with black:
 cbbPalette <- c( "#E69F00", "#009E73", "#F0E442", "#D55E00", "#CC79A7","#000000")
 
 
 mds_plot <- mds_df %>% 
-  ggplot() +
-  geom_point(aes(x = V1, y = V2, color = Macroarea)) +
+  ggplot(aes(x = V1, y = V2)) +
+  geom_point() +
+  geom_text(aes(label = Label)) +
   theme_minimal() +
   coord_fixed() +
   scale_colour_manual(values=cbbPalette)
