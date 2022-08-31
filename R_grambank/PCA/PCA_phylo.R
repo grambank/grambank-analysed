de74cb886eb8110f8fe97d9d1735d034d5a3fed0
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-08-31 16:51:03 +1200

    Update PCA_phylo.R

--- R_grambank/PCA/PCA_phylo.R
+++ R_grambank/PCA/PCA_phylo.R
@@ -21,11 +21,20 @@ GB_imputed <- GB_imputed[tree$tip.label,]
 
 ppca_obj <- phytools::phyl.pca(tree = tree, Y = GB_imputed,method ="BM", mode="cov")
 
-ppca_obj$S %>% 
+
+  ppca_obj$S %>% 
   as.data.frame() %>% 
   rownames_to_column("Language_ID") %>% 
   write_tsv("output/PCA/ppca_language_values.tsv")
 
+t(summary(ppca_obj)$importance) %>%
+    as.data.frame() %>%
+    rownames_to_column("PC") %>%
+    mutate(`Proportion of Variance` = round(`Proportion of Variance`, digits = 2)) %>%
+    dplyr::select(`Proportion of Variance`, everything()) %>% 
+  write_tsv("output/PCA/ppca_importance.tsv")
+
+  
 ppca_obj$L %>% 
   as.data.frame() %>% 
   rownames_to_column("Parameter_ID") %>% 
