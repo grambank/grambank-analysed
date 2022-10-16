1b619c484015fac0c473ab13fae8fce341da9d33
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-10-16 12:53:57 +0200

    Update requirements_fr.R

--- R_grambank/functional_richness/requirements_fr.R
+++ R_grambank/functional_richness/requirements_fr.R
@@ -2,10 +2,12 @@ source("requirements.R")
 
 #installing and loading extra packages for Simon and Damian's code for functional richness.
 
-h_load(verbose = F, pkg = c("fundiversity", "mFD"  ))
+h_load(verbose = F, pkg = c("fundiversity", "mFD" , "ggforce", "concaveman", "cowplot"))
 
 OUTPUTDIR <- "output/functional_richness"
 
 if(!dir.exists(OUTPUTDIR)){
   dir.create(OUTPUTDIR)
 }
+
+source("functional_richness/fun_def_make_group_matrix.R")
