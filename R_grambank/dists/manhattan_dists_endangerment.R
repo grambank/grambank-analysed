17f165b758cb019628f0877e5937451021478c43
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-10-17 13:56:10 +0200

    Update manhattan_dists_endangerment.R

--- R_grambank/dists/manhattan_dists_endangerment.R
+++ R_grambank/dists/manhattan_dists_endangerment.R
@@ -35,6 +35,30 @@ GB_dist_list %>%
   geom_point(aes(x = value, y = n, col = value)) +
   theme_classic() +
   theme(axis.title = element_blank(), 
-        legend.position = "None") 
+        legend.position = "None") +
+    xlab("Manhattan distance") +
+    ylab("n")
+
+GB_dist_list_binned <- GB_dist_list %>% 
+  transform(bin = cut(value, 40)) %>% #grouping the mean scores into 20 bins		
+  group_by(bin) %>% 		
+  dplyr::summarize(n = n())	%>% 
+  separate(bin, into = c("order", "second"), sep = ",", remove = F) %>% 
+  mutate(order = str_replace_all(order, "\\(", "") %>% as.numeric() %>% round(digits = 0)) %>% 
+  mutate(second = str_replace_all(second, "[\\(|\\[|\\]]", "") %>% as.numeric() %>% round(digits = 0)) %>% 
+  unite("order", "second",col = "bin_display", sep = " - ", remove = F, )
+ 
+
+GB_dist_list_binned$bin_display <- fct_reorder(GB_dist_list_binned$bin_display, GB_dist_list_binned$order)
+
+GB_dist_list_binned %>% 
+  ggplot() +
+  geom_bar(aes(x = bin_display, y = n, fill = order), stat = "identity") +
+  theme_classic() +
+  theme(legend.position = "None") +
+  xlab("Manhattan distance") +
+  ylab("n") +
+  viridis::scale_fill_viridis(discrete = F) +
+  theme(axis.text.x = element_text(angle = 65, hjust = 1)) 
 
 ggsave("output/dists/plot_manhattan_dists.png", height = 4, width = 5)
