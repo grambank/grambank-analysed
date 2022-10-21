408d59c3e14889ee74e264872b650ffc1844975b
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-10-21 16:16:06 +0200

    Update features_americas.R

--- R_grambank/features_americas.R
+++ R_grambank/features_americas.R
@@ -5,7 +5,7 @@ Language_meta_data <-  read_tsv("output/non_GB_datasets/glottolog-cldf_wide_df.t
   mutate(americas = ifelse(str_detect(Macroarea, "merica"), "americas", "not americas"))
 
 #reading in GB
-GB <- read.delim("output/GB_wide/GB_cropped_for_missing.tsv", sep ="\t") %>% 
+GB_americas_prop <- read.delim("output/GB_wide/GB_cropped_for_missing.tsv", sep ="\t") %>% 
   reshape2::melt(id.vars = "Language_ID") %>% 
   left_join(Language_meta_data) %>% 
   filter(!is.na(value)) %>% 
@@ -15,6 +15,19 @@ GB <- read.delim("output/GB_wide/GB_cropped_for_missing.tsv", sep ="\t") %>%
   group_by(variable) %>% 
   mutate(sum = sum(n)) %>% 
   ungroup() %>% 
-  mutate(prop = n / sum) %>% 
-  group_by(variable) %>% 
-  mutate(max = which.max(n))
+  mutate(prop = n / sum)
+
+
+#Shorter names for the Grambank parameters that can be used in graphs
+Grambank_id_abbrev <- read_tsv(
+  file.path("output", "GB_wide", "parameters_binary.tsv"),
+  show_col_types=FALSE) %>% 
+  rename(variable = ID)
+
+
+GB_americas_prop %>% 
+  group_by(variable) %>%
+  slice_max(n) %>% 
+  filter(americas == "americas") %>% 
+  left_join(Grambank_id_abbrev) %>% 
+  dplyr::select(Feature_ID = variable, Grambank_ID_desc, Name)
