1542249f799aa197dd86ddc2c4537be9ac426759
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-11-07 11:26:09 +0100

    Update make_wide.R

--- R_grambank/make_wide.R
+++ R_grambank/make_wide.R
@@ -1,6 +1,7 @@
 source("fun_def_h_load.R")
 h_load("tidyverse")
 source("global_variables.R")
+source("set_random_seed.R")
 
 
 #The CLDF-format is long, but for PCA, imputation and distances it is better with wide formatted data. 
