53abde4829ecf69efad90114bd0b86c9f9d9d931
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-11-11 18:08:04 +0100

    Update fixation_scores_muthukrishna_barplot.R

--- R_grambank/dists/fixation_scores_muthukrishna_barplot.R
+++ R_grambank/dists/fixation_scores_muthukrishna_barplot.R
@@ -21,26 +21,8 @@ mean(cfx_macroarea_list$Value_cfx)
 
 ggsave(filename = file.path(OUTPUTDIR, "cfx_barplot_macroarea.png"), height =  7.89, width =  8.61)
 
-fns <- list.files(path = OUTPUTDIR, pattern = ".*cfx_Family_ID_list_cut_off.*")
 
-for(fn in fns){
 
-cfx_Family_ID_list <- read_tsv(fn)
-
-plot_title <- fn
-
-cfx_Family_ID_list %>% 
-  ggplot(aes(x = Vars, y = Value_cfx)) +
-  geom_bar(aes(fill = 1 - Value_cfx), stat = "identity") +
-  theme_classic() +
-  theme(legend.position = "None", 
-        axis.title.x = element_blank(), 
-        axis.ticks.x = element_blank(), 
-        axis.text.x = element_blank()) +
-  ggtitle(plot_title) 
-
-mean(cfx_Family_ID_list$Value_cfx)
-
-ggsave(filename = paste0(OUTPUTDIR, "cfx_barplot", fn, ".png"), height =  7.89, width =  8.61)
-
-}
+cfx_macroarea_list %>%
+  separate(Vars, into = c("Var1", "Var2"), sep = " - ") %>%
+  reshape2::dcast(Var1 ~ Var2) %>% View()
