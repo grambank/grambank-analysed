28316eb5c128257f295d5b86d064a5d5fd22f591
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-11-11 19:48:13 +0100

    Update fixation_scores_muthukrishna_barplot_heatmap.R

--- R_grambank/dists/fixation_scores_muthukrishna_barplot_heatmap.R
+++ R_grambank/dists/fixation_scores_muthukrishna_barplot_heatmap.R
@@ -21,8 +21,31 @@ mean(cfx_macroarea_list$Value_cfx)
 
 ggsave(filename = file.path(OUTPUTDIR, "cfx_barplot_macroarea.png"), height =  7.89, width =  8.61)
 
+#heatmap
 
+cfx_macroarea_list_separated <- cfx_macroarea_list %>%
+  separate(Vars, into = c("Var1", "Var2"), sep = " - ")
 
-cfx_macroarea_list %>%
-  separate(Vars, into = c("Var1", "Var2"), sep = " - ") %>%
-  reshape2::dcast(Var1 ~ Var2) %>% View()
+#the dist list isn't symmetric, which isn't an issue really because we only want to display one half of the symmetric matrix, but currently it's not displaying one triangle consistently. So, we make it symmetric again and expicitly chop of one half
+h_load("igraph")
+g <- igraph::graph.data.frame(cfx_macroarea_list_separated, directed=FALSE)
+
+cfx_dist_matrix_sym <- igraph::get.adjacency(g, attr="Value_cfx", sparse=FALSE)
+
+cfx_dist_matrix_sym[upper.tri(cfx_dist_matrix_sym, diag = T)] <- NA
+
+#ggplot pipe for heatmap
+cfx_dist_matrix_sym %>% 
+  reshape2::melt() %>% 
+  filter(!is.na(value)) %>% 
+  ggplot(aes(Var1, Var2)) +
+  theme_classic() +
+  geom_tile(aes(fill = value), color='white', size = 0.6) +
+  scale_fill_viridis(direction = -1) +
+  geom_text(aes(label = round(value, 2)),color = "white", size = 8) +
+  theme(axis.title = element_blank(),
+        legend.position = "none", 
+        axis.text = element_text(size = 14)) +
+  scale_x_discrete(limits=rev)
+
+ggsave(file.path(OUTPUTDIR, "cfx_heatmap_macroarea.png"), height =  7.89, width =  8.61)
